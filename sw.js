eb49b664c9ba7656f71c3f664eaca2bf556d4cf0
austingillis85801 <austingillis85@gmail.com>
2025-09-02 15:17:10 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,26 @@
-const CACHE='eq-teachers-v1';
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/manifest.webmanifest'])))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+const CACHE = 'eq-teachers-v1';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) =>
+      cache.addAll([
+        // For GitHub Pages, don’t use leading slashes.
+        'manifest.webmanifest',
+      ])
+    )
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((match) => match || fetch(event.request))
+  );
+});
