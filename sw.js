fbacdc1af2e354aa469354cfb4d8b3ccb9dcf821
austingillis85801 <austingillis85@gmail.com>
2025-09-03 18:51:01 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,19 @@
-const CACHE = 'eq-teachers-v5';
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE).then((cache) =>
-      cache.addAll(['manifest.webmanifest'])
-    )
-  );
+// very light cache; safe to keep or remove (v3)
+const CACHE = 'eq-teach-v3';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll([
+    './',
+    './manifest.webmanifest'
+    // add icons if you have them: './icons/icon-192.png', './icons/icon-512.png'
+  ])));
 });
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
-    )
-  );
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
 });
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((m) => m || fetch(event.request))
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
